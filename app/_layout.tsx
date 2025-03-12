7966e23694996079f263eb503f15aee4ded3bcc6
정민우 소프트웍스 <sae1013@gmail.com>
2025-03-13 00:32:39 +0900

    [FEAT] 웹뷰 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,8 +9,10 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
+import { SafeAreaView, StyleSheet } from "react-native";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import WebView from "react-native-webview";
+// import { SafeAreaView,  } from "react-native-safe-area-context";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -33,11 +35,20 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <SafeAreaView style={styles.safeArea}>
+        <WebView source={{ uri: "http://192.168.219.102:3000/search" }} />
+      </SafeAreaView>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#fff", // 필요에 따라 배경색 지정
+  },
+  webview: {
+    flex: 1,
+  },
+});
